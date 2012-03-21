a023a78c972acab95b155997e79a25a30d32f59f
louwbc <lizkca@gmail.com>
2012-03-21 19:16:15 +0800

    models.py

--- apps/record/models.py
+++ apps/record/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+# python
+import os
+from os import path
+from datetime import datetime
 
-# Create your models here.
+# django imports
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
